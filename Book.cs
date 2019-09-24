bbc655642c23c11819cb78255696a2c0d2958dd0
Alibek <52044412+Alibek15@users.noreply.github.com>
2019-09-25 01:02:33 +0600

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{ [Serializable]
+    public class Book
+    {
+       
+       
+            public string Name { get; set; }
+            public int Price { get; set; }
+            public string AuthorName { get; set; }
+            public DateTime Year { get; set; }
+        
+    }
+}
---
+++ Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    [Serializable]
+    public class Login
+    {
+        public string Account { get; set; }
+        public string Password { get; set; }
+
+    }
+}
